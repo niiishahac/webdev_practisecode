9599f5df429513e6ba9a5d14eda1da6528afddec
Nisha Kumari <nisha.23b1541076@abes.ac.in>
2025-08-28 00:14:33 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,31 @@
+console.log("HELLO");
+console.log(3);
+// ----variable declaration ----
+a = 10;
+console.log(a);
+a = 20.7;
+console.log(a);
+// a++ and ++a
+a = a + 4;
+console.log(a);
+a = 9;
+b = 5;
+c = 8;
+console.log(a, b, c);
+// -------
+age = 24;
+console.log("my age is" + " " + age);
+// var can  be updated or changed but const cannot be changed
+var z = 4;
+console.log(z); //4
+var z = "java";
+console.log(z); //java
+// ------------------------
+const w = 4;
+console.log(w); //4
+// const w="nisha"; -> error ka baad ka part run nahi hoga
+// ---and var variable ko const nahi kar sakta baad mein
+var z = 4;
+console.log(z);
+const z = "java";
+console.log(z); //error
